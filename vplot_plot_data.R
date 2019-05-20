d9cf0187e861deac7415140fba3fe3e9a6b58f65
Elise Bell <eabell2@utep.edu>
2019-05-20 10:55:34 -0600

    5/20 adding step by step plot commands

--- vplot_plot_data.R
+++ vplot_plot_data.R
@@ -1,8 +1,169 @@
+# These are the R packages you need to run this code correctly.
 require(ggplot2)
 require(ggpubr)
 require(plyr)
 require(dplyr)
 require(reshape2)
+
+# If you're running this after running vplots_read_data.R, you will already have a dataframe called 'data' in your environment
+
+
+
+# Assuming we want to plot a F1 ~ F2 vowel space, we first need to tell ggplot2 what data to look at
+ggplot(data=data)
+
+# We also need to tell it what factors it should pay attention to
+ggplot(data=data, aes(x=f2_50, y=f1_50, label=vowel))
+
+# If you run that code, you get a plot, but no data appears
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_point()
+
+# Now we have data, but no information about what vowels it's from
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_point(aes(color=vowel))
+
+# Something doesn't look quite right - /u/ is in the bottom left, but if we want a traditional vowel plot, it should be in the top right
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_point(aes(color=vowel)) +
+  scale_x_reverse() +
+  scale_y_reverse()
+
+# Now our data looks like it's in the right place (we've reversed the x and y axes). If we want the axis labels to be on the top and right, we just have to add position modifiers to the scale_ commands.
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_point(aes(color=vowel)) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position = "right")
+
+# We can also indicate vowel identity using shape instead of color.
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_point(aes(shape=vowel)) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position = "right")
+
+# We can also plot the vowel symbols, using geom_text() instead of geom_point()
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_text(aes(label=vowel)) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position = "right")
+
+# You can combine geom_text() and color
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_text(aes(label=vowel, color=vowel)) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position = "right")
+
+# You may want to include both raw and summarized data. To summarize your data, you can use the reshape2 packages (for pipes %>%) and dplyr
+# This command produces a new dataframe (actually a tibble, don't worry about that) with one value for combination of vowel and v_length
+# It summarizes across speakers and across any other variables that aren't included in the group_by() command
+# Here, I only calculate means, but you can make many other summary calculations if desired.
+data.means <- data %>%
+  group_by(vowel, v_length) %>%
+  summarise(f1_50 = mean(f1_50),
+            f2_50 = mean(f2_50))
+
+# Once you have that summarized, data you can add it to the plot containing the unsummarized data
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_text(aes(label=vowel, color=vowel)) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position = "right") +
+  geom_text(data=data.means, aes(label=vowel), size=12)
+
+# You can vary the transparency of layers of data by setting alpha to a value between 0 and 1. The smaller alpha is, the more transparent the result.
+ggplot(data=data, aes(x=f2_50, y=f1_50)) +
+  geom_text(aes(label=vowel, color=vowel)) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position = "right") +
+  geom_text(data=data.means, alpha=0.5, aes(label=vowel), size=12)
+
+# Here are some other useful ggplot commands.
+
+# To hide a legend that you don't want, try adding show.legend=FALSE to the geom_ call.
+ggplot(data=data, aes(x=vowel, y=duration)) + geom_boxplot(aes(color=v_length))
+ggplot(data=data, aes(x=vowel, y=duration)) + geom_boxplot(aes(color=v_length), show.legend=FALSE)
+
+# To change the font size of axis and legend text, use the theme() command
+ggplot(data=data, aes(x=vowel, y=duration)) + 
+  geom_boxplot(aes(color=v_length)) +
+  theme(axis.text=element_text(size=24),
+      axis.title=element_text(size=24),
+      strip.text=element_text(size=24),
+      legend.title=element_text(size=24),
+      legend.text=element_text(size=24))
+
+# To change the default names of the x and y axes add
+ggplot(data=data, aes(x=vowel, y=duration)) + 
+  geom_boxplot(aes(color=v_length)) +
+  ylab("Duration (ms)") + 
+  xlab("VOWELS")
+
+# To specify the limits of the axes, add limits, to the call to scale_x_()
+ggplot(data=data, aes(x=vowel, y=duration)) + 
+  geom_boxplot(aes(color=v_length)) +
+  scale_y_continuous(limits = c(0,500))
+
+# Summarize data by calculating mean values for each vowel produced by each speaker
+data.summary <- data %>%
+  group_by(subject, vowel, v_length) %>%
+  summarize(f1_norm = mean(f1_norm),
+            f2_norm = mean(f2_norm),
+            duration = mean(duration),
+            f0_50 = mean(f0_50),
+            f1_50 = mean(f1_50),
+            f2_50 = mean(f2_50),
+            f3_50 = mean(f3_50))
+
+# Summarize data by calculating mean values for each vowel (across speakers) at each level of the 'v_length' variable
+data.means <- data %>%
+  group_by(vowel, v_length) %>%
+  summarise(f1_norm = mean(f1_norm),
+            f2_norm = mean(f2_norm),
+            duration = mean(duration),
+            f0_50 = mean(f0_50),
+            f1_50 = mean(f1_50),
+            f2_50 = mean(f2_50),
+            f3_50 = mean(f3_50))
+
+
+# Vowel duration
+ggplot(data=data.summary, aes(vowel, duration)) +
+  geom_boxplot(outlier.shape = 1, position="dodge", aes(color=v_length), show.legend=FALSE) +
+  ylab("Duration (ms)") + xlab("Vowel") +
+  labs(lty="Vowel") +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text=element_text(size=12),
+        legend.text=element_text(size=10)) +
+  ggtitle("Vowel duration")
+
+# plot with one dot per subject per vowel - normalized data
+ggplot(data=data.summary, aes(x=f2_norm, y=f1_norm, label=vowel)) +
+  geom_point(alpha=0.25, aes(color=vowel), size=10, show.legend=FALSE) + 
+  geom_text(data=data.means, size=10, show.legend=FALSE) +
+  scale_color_discrete(guide=FALSE) +
+  ylab("Normalized F1") + xlab("Normalized F2") +
+  labs(lty="Vowel") +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text=element_text(size=12),
+        legend.text=element_text(size=10)) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position = "right")
+
+# plot with one dot per subject per vowel - non-normalized data
+ggplot(data=data.summary, aes(x=f2_50, y=f1_50, label=vowel)) +
+  geom_point(alpha=0.25, aes(color=vowel, shape=subject), size=10, show.legend=FALSE) + 
+  facet_wrap(.~subject)
+  geom_text(data=data.means, size=10, show.legend=FALSE) +
+  scale_color_discrete(guide=FALSE) +
+  ylab("F1 (hz)") + xlab("F2 (hz)") +
+  labs(lty="Vowel") +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text=element_text(size=12),
+        legend.text=element_text(size=10)) +
+  scale_x_reverse(position = "top", limits = c(3000, 500)) +
+  scale_y_reverse(position = "right", limits = c(1000, 200))
 
 ##### # plot WS data ##### 
 ws.fp <- droplevels(data[data$group=="WS",])
@@ -100,18 +261,7 @@ ggplot(data=ws.fp.2[ws.fp.2$vowel %in% c("i:", "ɨ:", "e:", "i", "ɨ", "e"),], a
   scale_y_reverse(position = "right") + #, limits = c(1000, 200)) +
   ggtitle("Patagonian Welsh")
 
-# WS duration
-ggplot(data=ws.fp[!is.na(ws.fp$syll_type2) & ws.fp$vowel %in% c("i:", "ɨ:", "e:", "i", "ɨ", "e"),], aes(vowel, duration)) +
-  #geom_boxplot()
-  geom_boxplot(outlier.shape = 1, position="dodge", aes(color=length), show.legend=FALSE) +
-  #facet_wrap(~unpred, nrow=1, scales="free_x") +
-  ylab("Duration (ms)") + xlab("Vowel") +
-  labs(lty="Vowel") +
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=12),
-        strip.text=element_text(size=12),
-        legend.text=element_text(size=10)) +
-  ggtitle("Patagonian Welsh")
+
 
 
 ##### # plot WE data ##### 
@@ -215,7 +365,7 @@ ggplot(data=we.fp.2[we.fp.2$vowel %in% c("i:", "ɨ:", "e:", "i", "ɨ", "e"),], a
 # WE duration
 ggplot(data=we.fp[!is.na(we.fp$syll_type2) & we.fp$vowel %in% c("i:", "ɨ:", "e:", "i", "ɨ", "e"),], aes(vowel, duration)) +
   #geom_boxplot()
-  geom_boxplot(outlier.shape = 1, position="dodge", aes(color=length), show.legend=FALSE) +
+  geom_boxplot(outlier.shape = 1, position="dodge", aes(color=v_length), show.legend=FALSE) +
   #facet_wrap(~unpred, nrow=1, scales="free_x") +
   ylab("Duration (ms)") + xlab("Vowel") +
   labs(lty="Vowel") +
--- vplots_read_data.R
+++ vplots_read_data.R
@@ -1,16 +1,29 @@
-require(ggplot2)
-require(ggpubr)
-require(plyr)
-require(dplyr)
-require(reshape2)
-
 # Read in data from a CSV
+
+# sample data contains the following columns:
+#  [1] "subject"  "word"     "vowel"    "start"    "end"      "duration" "f0_20"    "f0_50"    "f0_80"    "f1_20"    "f1_40"    "f1_50"    "f1_60"   
+# [14] "f1_80"    "f2_20"    "f2_40"    "f2_50"    "f2_60"    "f2_80"    "f3_20"    "f3_40"    "f3_50"    "f3_60"    "f3_80"    "age"      "sex"     
+# [27] "f1_norm"  "f2_norm"  "f3_norm"  "v_length"
+
+# Minimally, your data should have the following columns ("duration", "f0_50", and "f3_50" are optional as well, if you only want to plot F1 ~ F2):
+#  [1] "subject"  "word"     "vowel"    "duration" "f0_50""f1_50"    "f2_50"    "f3_50"
+
+# You should also include columns for any additional categorical information you have about your data (for example, tone or phonemic length)
+
 data <- read.csv("voweldata.csv", header=T, encoding = "UTF-8")
-data$vowel <- gsub("?", "ɨ", data$vowel, fixed=TRUE)
+
+# Check that all vowel symbols were read in correctly
+unique(data$vowel)
+
+# Use regular expressions and gsub() to correct any mis-read values
+data$vowel <- gsub("?", "ɨ", data$vowel, fixed=TRUE) # fixed=TRUE means that characters should be interpreted literally, not using regex interpretations
 data$vowel <- gsub("<U+0259>", "ə", data$vowel, fixed=TRUE)
 
-
+# Make sure that the vowel column acts as a factor, not as character strings (do the same for any other categorical variables in your data set)
 data$vowel <- as.factor(data$vowel)
 
+# Look at the first six rows to make sure that everything looks okay
 head(data)
+
+# Use summary() to get a summary of the entire dataframe, or a single column
 summary(data$vowel)
